e15b35f6c89452ac9f3f3be0b217a0ea676e1eda
Wero Mani <9087weronika@gmail.com>
2021-04-17 21:22:48 +0200

    first commit

---
+++ gifts.py
@@ -0,0 +1,8 @@
+class Gifts:
+    def __init__(self, color, size):
+        self.color = color
+        self.size = size
+
+gift_1 = Gifts("red", "big")
+
+print(gift_1.size.color)
---
+++ main.py
@@ -0,0 +1,33 @@
+from gifts import Gifts
+
+
+# print("Witaj w  organizatorze poprawnej katolickiej wigilii. \nJest 23 grudnia. \nZaczynamy!")
+#
+#
+#
+#
+#
+#
+#
+# guests = (int(input("Wprowadź liczbę gości: ")))
+# # nie można wprowadzić str ale program nie działa potem
+#
+# if guests < (int(5)):
+# # Nie można dodać 0,5 goscia, ALE program nie działa potem
+#     print("Cudownie. Tak małe grono ochroni wszystkich od zarażenia się SARS_COV-19.")
+#
+# else:
+#     print("Premier Morawiecki nie zezwala na to!")
+#     input("Wprowadź poprawną liczbę gości wg najnowszych obostrzeń: ")
+
+
+# print('Chcesz choinke?')
+# for i in range(1,20,2):
+#     print(('*'*i).center(20))
+#
+# for leg in range(3):
+#     print(('||').center(20))
+#
+# print(('\====/').center(20))
+
+print(gift_1)
